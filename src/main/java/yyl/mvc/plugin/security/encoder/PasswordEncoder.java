493a49b8bf3a5f0f84cccd78fe6356bc459f0f4b
yyl <relucent@163.com>
2025-11-12 10:23:36 +0800

    #

--- src/main/java/yyl/mvc/plugin/security/encoder/PasswordEncoder.java
+++ src/main/java/yyl/mvc/plugin/security/encoder/PasswordEncoder.java
@@ -7,18 +7,18 @@ package yyl.mvc.plugin.security.encoder;
  */
 public interface PasswordEncoder {
 
-	/**
-	 * 对原始密码进行加密
-	 * @param rawPassword 原始密码
-	 * @return 加密后的字符串
-	 */
-	String encode(CharSequence rawPassword);
+    /**
+     * 对原始密码进行编码
+     * @param rawPassword 原始密码
+     * @return 编码后的字符串
+     */
+    String encode(CharSequence rawPassword);
 
-	/**
-	 * 匹配密码明文和密文
-	 * @param rawPassword 原始密码(明文)
-	 * @param encodedPassword 加密后的密码(密文)
-	 * @return 如果明文和密文匹配则返回true,否则返回false
-	 */
-	boolean matches(CharSequence rawPassword, CharSequence encodedPassword);
+    /**
+     * 验证从存储中获取的编码密码是否与提交的原始密码匹配
+     * @param rawPassword     要进行编码和匹配的原始密码
+     * @param encodedPassword 来自存储的已编码密码，用于比较
+     * @return 如果原始密码在编码后与来自的编码密码匹配则返回true，否则返回false
+     */
+    boolean matches(CharSequence rawPassword, CharSequence encodedPassword);
 }
